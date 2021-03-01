cb64bca0918a371ef2fe5c44a1e970c43f85e65b
concussion20 <zhou.chang1@husky.neu.edu>
2021-03-01 01:14:26 -0800

    mini assignment 5 (global variable is not optimal way to save counter value)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+let count = 0;
+
+class Child extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isOff: props.isOff};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    count += this.state.isOff ? 1 : -1;
+    this.setState(prevState => ({
+      isOff: !prevState.isOff
+    }));    
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleClick} className={`child ${this.state.isOff ? 'white': 'black'}`}>
+      </div>
+    );
+  }
+
+}
+
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: count};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({count: count});
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Count: {this.state.count}</h2>
+        <div onClick={this.handleClick} className='grid'>
+          <Child isOff/>
+          <Child isOff/>
+          <Child isOff/>
+          <Child isOff/>
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Parent />
     </div>
   );
 }
